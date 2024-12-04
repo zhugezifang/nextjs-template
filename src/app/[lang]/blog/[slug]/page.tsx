123e1af951220bf3961683c6378604a96031f831
zhugezifang <xiaoqiu2017wy@163.com>
2024-12-04 14:53:32 +0000

    feat:blog

--- src/app/[lang]/blog/[slug]/page.tsx
+++ src/app/[lang]/blog/[slug]/page.tsx
@@ -20,13 +20,13 @@ export default async function BlogPost({ params }: { params: { slug: string, lan
   }
 
   return (
-    <main className="container py-12 md:py-24">
+    <main className="container">
 
       <div className={"w-[100%] mx-auto rounded-tl-[30px] rounded-tr-[30px] object-fill"} >
            <iframe src="https://combinations.org/" allow="autoplay"  style={{ top: '0px', left: '0px',width: '100%', height: '1000px'}}></iframe>
       </div>
 
-      <article className="prose prose-gray dark:prose-invert mx-auto">
+      <article className="prose prose-gray dark:prose-invert mx-auto py-4">
         <h1 className="mb-4">{post.title}</h1>
         <div className="flex items-center text-gray-500 mb-8">
           <span>{post.date}</span>
