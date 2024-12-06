bce91714dee8d0ecac0bd87665a11c55b3719147
zhugezifang <xiaoqiu2017wy@163.com>
2024-12-06 06:40:57 +0000

    feat:btn

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,7 +1,7 @@
-import { Button } from "@/components/ui/button"
-import { Chrome } from "lucide-react"
+//import { Button } from "@/components/ui/button"
+//import { Chrome } from "lucide-react"
 //import Features from "@/components/features"
-import Hero from "@/components/hero"
+//import Hero from "@/components/hero"
 //import FAQ from "@/components/faq"
 import { getDictionary } from '@/i18n/get-dictionary'
 import type { Locale } from '@/i18n/config'
@@ -20,7 +20,7 @@ export default async function Home({
   return (
     <main className="flex flex-col items-center w-full">
 
-      <div className="w-full py-10 px-4">
+      <div className="container py-10 px-4">
           
           <div className="flex items-center justify-between mb-6">
             <h1 className="text-3xl font-bold">热门FC</h1>
@@ -42,22 +42,7 @@ export default async function Home({
           
       </div>
 
-      <section className="w-full py-12 md:py-24 lg:py-32">
-        <div className="container px-4 md:px-6">
-          <div className="flex flex-col items-center space-y-4 text-center">
-            <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
-              {dict.home.cta.title}
-            </h2>
-            <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-              {dict.home.cta.description}
-            </p>
-            <Button size="lg" className="mt-4">
-              <Chrome className="mr-2 h-5 w-5" />
-              {dict.home.cta.button}
-            </Button>
-          </div>
-        </div>
-      </section>
+      
     </main>
   )
 }
